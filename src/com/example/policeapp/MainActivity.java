2a164cfe96fafefaefa591821e855555361c0c5f
vikishim <viki_shim@163.com>
2014-11-06 17:45:48 +0800

    Add custom GridView

--- src/com/example/policeapp/MainActivity.java
+++ src/com/example/policeapp/MainActivity.java
@@ -6,17 +6,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.policeapp.common.ImageUtil;
 import com.example.policeapp.common.TitleBar;
+import com.example.policeapp.view.MenuGridView;
 
 public class MainActivity extends Activity {
+
+	private int mPendingCount = 3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +35,14 @@ public class MainActivity extends Activity {
 	}
 
 	private void configBody() {
-		GridView gridView = (GridView) findViewById(R.id.grid_view);
+		MenuGridView gridView = (MenuGridView) findViewById(R.id.grid_view);
 		gridView.setAdapter(new MenuAdapter(this));
-		gridView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v,
-					int position, long id) {
-				Toast.makeText(MainActivity.this, "" + position,
-						Toast.LENGTH_SHORT).show();
-			}
-		});
 	}
 
 	class MenuAdapter extends BaseAdapter {
-		
+
 		private Context mContext;
-		
+
 		public MenuAdapter(Context context) {
 			this.mContext = context;
 		}
@@ -70,19 +64,45 @@ public class MainActivity extends Activity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			ViewHolder hodler;
-			if(convertView == null){
-				hodler = new ViewHolder();
-				convertView = LayoutInflater.from(mContext).inflate(R.layout.menu_item, null);
-				hodler.img = (ImageButton) convertView.findViewById(R.id.img_btn);
-				hodler.label = (TextView) convertView.findViewById(R.id.label);
-				convertView.setTag(hodler);
-			}else{
-				hodler = (ViewHolder) convertView.getTag();
+			ViewHolder holder;
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = LayoutInflater.from(mContext).inflate(
+						R.layout.menu_item, null);
+				holder.img = (ImageButton) convertView
+						.findViewById(R.id.img_btn);
+				holder.label = (TextView) convertView.findViewById(R.id.label);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
 			}
-			hodler.img.setImageResource(mImageIds[position]);
-			hodler.label.setText(mLabelIds[position]);
+			if (1 == position) {
+				ImageUtil imgUtil = ImageUtil.getInstance();
+				imgUtil.setContext(mContext);
+				holder.img.setImageBitmap(imgUtil.drawCountIcon(
+						imgUtil.drawableToBitmap(getResources().getDrawable(
+								mImageIds[position])), mPendingCount));
+			} else {
+				holder.img.setImageResource(mImageIds[position]);
+			}
+			holder.label.setText(mLabelIds[position]);
+			holder.img.setOnClickListener(new ImgBtnClickListener(position));
 			return convertView;
+		}
+
+		class ImgBtnClickListener implements View.OnClickListener {
+
+			private int mPosition;
+
+			ImgBtnClickListener(int pos) {
+				this.mPosition = pos;
+			}
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "" + mPosition, Toast.LENGTH_SHORT)
+						.show();
+			}
 		}
 
 	}
